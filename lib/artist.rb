99574038a8a4539c5406189761d213988c113a7f
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-18 02:17:59 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist
-  attr_accessor :name
-
+  attr_accessor :name, :songs
   @@song_count = 0
 
   def initialize(name)
@@ -8,24 +7,25 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)
+  def add_song(new_song)
+    @songs << new_song
+    #song.artist = self
+  #  @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = song.new(name)
     @songs << song
     song.artist = self
-    @@song_count +=1
+    @@song_count += 1
+    song
   end
 
-  def add_song_by_name(song)
-    song=Song.new(song)
-    @songs << song
-    song.artist=self
-    @@song_count +=1
-  end
+    #def songs
+    #  @songs
+  #  end
 
-  def songs
-    @songs
-  end
-
-  def self.song_count
-    @@song_count
-  end
+    def self.song_count
+      @@song_count
+    end
 end
