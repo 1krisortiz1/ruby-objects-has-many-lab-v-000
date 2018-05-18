8a8761510a6cfa5fd890a38f9774af030c0a924d
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-18 02:31:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,15 +10,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
-    song = song.new(name)
+    song = Song.new(name)
     @songs << song
-    song.artist = self
     @@song_count += 1
-    song
+    song.artist = self
   end
 
     def songs
